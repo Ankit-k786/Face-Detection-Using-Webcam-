279b77c68b676c3429cd05a6804a8cd5af4004aa
Ankit Katiyar <74397253+Ankit-k786@users.noreply.github.com>
2023-03-21 15:37:56 +0530

    Add files via upload

---
+++ src/constant.py
@@ -0,0 +1,20 @@
+"""
+File : constant.py
+Description : Constant defined in this class to use across the project
+Created on : 3-jan-2023
+Author :Ankit katiyar
+E-mail :katiyar786ankit@gmail.com
+"""
+
+FACE_DETECTOR_MODEL_PATH = './model/haarcascade_frontalface_default.xml'
+SCALE_FACOR = 1.1
+MIN_NEIGHBOUR = 5
+MIN_SIZE = (30, 30)
+ASSIGNMENT3_OUTPUT_PATH = './output/Assignment3_output.mp4'
+FPS = 20.0
+RESOLUTION = (640, 480)
+COLOR = (0, 0, 255)
+LOCATION = (5, 30)
+SCALE = 2
+THICKNESS = 1
+
